dad59967c376fadb1a491759149ca153a462e410
ginpei <ginpei@gmail.com>
2017-07-05 15:52:13 -0700

    store
    They may get removed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const Vue = require('vue')
 const App = require('./components/App.vue')
-// const store = require('./store/index.js')
+const store = require('./store/index.js')
 const router = require('./router.js')
 
 new Vue({  // eslint-disable-line no-new
 	el: '#app',
-	// store,
+	store,
 	router,
 	render: f=>f(App),
 })
